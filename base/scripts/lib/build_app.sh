6bb696d1ccfbb666f1c7763f77d735a4ad3495bb
Jesse Rosenberger <github@jro.cc>
2016-08-02 12:45:03 +0300

    meteor npm install to account for when environment is truly fresh (i.e. CI)

--- base/scripts/lib/build_app.sh
+++ base/scripts/lib/build_app.sh
@@ -8,6 +8,8 @@ BUNDLE_DIR=/tmp/bundle-dir
 cp -R /app $COPIED_APP_PATH
 cd $COPIED_APP_PATH
 
+meteor npm install --production
+
 meteor build --directory $BUNDLE_DIR --server=http://localhost:3000
 
 cd $BUNDLE_DIR/bundle/programs/server/
