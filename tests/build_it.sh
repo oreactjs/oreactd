34d41511c05199932d5867628eaeadaa6c26f3ad
Jesse Rosenberger <github@jro.cc>
2016-10-20 00:19:41 +0300

    Add -node-4.6.1 suffix to docker images

--- tests/build_it.sh
+++ tests/build_it.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 set -x
 
-docker build --no-cache -t abernix/meteord:base ../base
-docker build --no-cache -t abernix/meteord:onbuild ../onbuild
-docker build --no-cache -t abernix/meteord:devbuild ../devbuild
-docker build --no-cache -t abernix/meteord:binbuild ../binbuild
+docker build --no-cache -t abernix/meteord:base-node-4.6.1 ../base
+docker build --no-cache -t abernix/meteord:onbuild-node-4.6.1 ../onbuild
+docker build --no-cache -t abernix/meteord:devbuild-node-4.6.1 ../devbuild
+docker build --no-cache -t abernix/meteord:binbuild-node-4.6.1 ../binbuild
