4f15d865de09ccd669186f067cce9a3fe50e4cea
Jesse Rosenberger <github@jro.cc>
2016-10-19 23:41:17 +0300

    No cache on the builds

--- tests/build_it.sh
+++ tests/build_it.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-docker build -t meteorhacks/meteord:base ../base
-docker build -t meteorhacks/meteord:onbuild ../onbuild
-docker build -t meteorhacks/meteord:devbuild ../devbuild
-docker build -t meteorhacks/meteord:binbuild ../binbuild
+docker build --no-cache -t meteorhacks/meteord:base ../base
+docker build --no-cache -t meteorhacks/meteord:onbuild ../onbuild
+docker build --no-cache -t meteorhacks/meteord:devbuild ../devbuild
+docker build --no-cache -t meteorhacks/meteord:binbuild ../binbuild
