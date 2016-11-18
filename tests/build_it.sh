4343b5334ef8dfcd8eeb7ad483e068fdc53c97d2
Jesse Rosenberger <github@jro.cc>
2016-11-18 14:24:09 +0200

    Change docker tag format to version first and variation after.

--- tests/build_it.sh
+++ tests/build_it.sh
@@ -3,11 +3,11 @@ set -x
 
 : ${NODE_VERSION?"NODE_VERSION has not been set."}
 
-docker build --no-cache --build-arg "NODE_VERSION=${NODE_VERSION}" -t "abernix/meteord:base-node-${NODE_VERSION}" ../base && \
-  docker tag "abernix/meteord:base-node-${NODE_VERSION}" abernix/meteord:base
-docker build --no-cache --build-arg "NODE_VERSION=${NODE_VERSION}" -t "abernix/meteord:onbuild-node-${NODE_VERSION}" ../onbuild && \
-  docker tag "abernix/meteord:onbuild-node-${NODE_VERSION}" abernix/meteord:onbuild
-docker build --no-cache --build-arg "NODE_VERSION=${NODE_VERSION}" -t "abernix/meteord:devbuild-node-${NODE_VERSION}" ../devbuild && \
-  docker tag "abernix/meteord:devbuild-node-${NODE_VERSION}" abernix/meteord:devbuild
-docker build --no-cache --build-arg "NODE_VERSION=${NODE_VERSION}" -t "abernix/meteord:binbuild-node-${NODE_VERSION}" ../binbuild && \
-  docker tag "abernix/meteord:binbuild-node-${NODE_VERSION}" abernix/meteord:binbuild
+docker build --build-arg "NODE_VERSION=${NODE_VERSION}" -t "abernix/meteord:node-${NODE_VERSION}-base" ../base && \
+  docker tag "abernix/meteord:node-${NODE_VERSION}-base" abernix/meteord:base
+docker build --build-arg "NODE_VERSION=${NODE_VERSION}" -t "abernix/meteord:node-${NODE_VERSION}-onbuild" ../onbuild && \
+  docker tag "abernix/meteord:node-${NODE_VERSION}-onbuild" abernix/meteord:onbuild
+docker build --build-arg "NODE_VERSION=${NODE_VERSION}" -t "abernix/meteord:node-${NODE_VERSION}-devbuild" ../devbuild && \
+  docker tag "abernix/meteord:node-${NODE_VERSION}-devbuild" abernix/meteord:devbuild
+docker build --build-arg "NODE_VERSION=${NODE_VERSION}" -t "abernix/meteord:node-${NODE_VERSION}-binbuild" ../binbuild && \
+  docker tag "abernix/meteord:node-${NODE_VERSION}-binbuild" abernix/meteord:binbuild
