846e16b8a4425a30482440de39ad0d1c4d13841b
vinay <vinay.kevadiaa@gmail.com>
2020-06-25 09:07:47 +0530

    autoconf dependency installation for some npm packages.

--- base/scripts/lib/install_base.sh
+++ base/scripts/lib/install_base.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 set -e
 apt-get update -y
-apt-get install -y curl bzip2 build-essential g++ python git apt-transport-https ca-certificates
+apt-get install -y curl bzip2 build-essential autoconf libtool pkg-config nasm gcc g++ python git apt-transport-https ca-certificates
